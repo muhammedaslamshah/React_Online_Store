e404da900b3282f72c3ad083ab4e85eae1ba2200
muhammedaslamshah <muhammedaslamshah700@gmail.com>
2023-12-23 22:35:40 +0530

    1st commit | setup product list page

---
+++ src/components/Products/product.data.js
@@ -0,0 +1,28 @@
+export const phonesData =[ {
+    id: 1,
+    model: "Apple I Phone",
+    desc: "80000",
+    image: "apple",
+    qty: 1,
+},
+{
+    id: 2,
+    model: "Samsung Fold",
+    desc: "80000",
+    image: "samsung",
+    qty: 1,
+},
+{
+    id: 3,
+    model: "Google Pixel",
+    desc: "80000",
+    image: "pixel",
+    qty: 1,
+},
+{
+    id: 4,
+    model: "Sumsung 23",
+    desc: "80000",
+    image: "samsungs23",
+    qty: 1,
+}];
